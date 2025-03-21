594d1b49678adb570c93373ea04c76857f2905ab
albertsilvawd <sil24011@byui.edu>
2025-03-21 15:18:09 -0300

    W03_Encap

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,57 @@
+public class Fraction
+{
+    // Atributos privados (encapsulados)
+    private int _numerator;
+    private int _denominator;
+
+    // Construtores
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    // Getters e Setters
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+
+    public void SetDenominator(int denominator)
+    {
+        _denominator = denominator;
+    }
+
+    // Métodos para representação
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator; // Cast para double para evitar divisão inteira
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -2,8 +2,26 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        // Testar construtores
+        Fraction f1 = new Fraction(); // 1/1
+        Fraction f2 = new Fraction(5); // 5/1
+        Fraction f3 = new Fraction(3, 4); // 3/4
+        Fraction f4 = new Fraction(1, 3); // 1/3
+
+        // Testar Getters e Setters
+        f1.SetNumerator(2);
+        f1.SetDenominator(2);
+        Console.WriteLine(f1.GetFractionString()); // Saída: 2/2
+        Console.WriteLine(f1.GetDecimalValue());   // Saída: 1.0
+
+        // Exibir resultados
+        Console.WriteLine(f2.GetFractionString()); // 5/1
+        Console.WriteLine(f2.GetDecimalValue());   // 5.0
+        Console.WriteLine(f3.GetFractionString()); // 3/4
+        Console.WriteLine(f3.GetDecimalValue());   // 0.75
+        Console.WriteLine(f4.GetFractionString()); // 1/3
+        Console.WriteLine(f4.GetDecimalValue());   // 0.3333333333333333
     }
 }
